443cd34c86a459fa461ccb5a4ee3fac094ac8cba
kibuniverse <3050540005@qq.com>
2020-02-20 00:08:12 +0800

    Changes to be committed: 	modified:   src/main/webapp/WEB-INF/web_project/compile_page.html        modified:   src/main/webapp/WEB-INF/web_project/index.html 	modified:   src/main/webapp/WEB-INF/web_project/personal_page.html 	modified:   src/main/webapp/WEB-INF/web_project/user_message_set.html 	modified:   src/main/webapp/js/index.js 	modified:   src/main/webapp/js/personal_page.js 	modified:   src/main/webapp/style/base_style.css 	modified:   src/main/webapp/style/index.css

--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -1,22 +1,65 @@
-var xuehao = '04181019'
+var xuehao = '04181019';
 
-window.onload =function (xuehao) {
+getFromAndroid = function(userid) {
+	window.userid = userid;
+}
+window.onload =function () {
 	const xhr = new XMLHttpRequest();
 	xhr.onreadystatechange
 }
 
 
+var res = {
+	status: 0,
+	msg: '注册成功',
+	data: {
+		id : null,
+		username : "04182077",
+		headSculpture : null,
+		messageCount : null,
+		fans : null,
+		concern : null,
+		readCount : null,
+		studentId : "04182077"
+	}
+} 
 
-var message = {
-	id : 1,
-	user_id : yankaizhi,
-	content: '这不是演习',
-	pageviews: 100,
-	praise_points: 200,
-	comment_count: 10,
-	time: new Date()
+
+class Obj{
+	constructor(nav_item) {
+		this.nav_item = nav_item;
+	}
+
+	init() {
+		this.nav_item.addEventListener('click', (e) => {
+			this.handleNavClick(e);
+		}, false);
+	}
+	// 渲染页面函数
+	render(data) {
+
+	}
+
+
+	handleNavClick(e) {
+		let target = e.target;
+
+		if(target.classList.contains('cur') || target == this.nav_item) {
+			// 如果未点击上按钮或点击的按钮已经被选择，则直接退出
+			return;
+		}
+
+		for(let i = 0; i < this.nav_item.childElementCount; i++) {
+			this.nav_item.childNodes[i].classList.remove('cur');
+		}
+
+		target.classList.add('cur');
+		//执行渲染函数
+		this.render();
+	}
 }
 
 
 
-
+var obj = new Obj(document.getElementsByClassName('nav_item')[0]);
+obj.init();
--- src/main/webapp/js/personal_page.js
+++ src/main/webapp/js/personal_page.js
@@ -10,3 +10,18 @@ class Obj {
 	}
 
 }
+
+var user_message = {
+	status: 0,
+	msg: '注册成功',
+	data: {
+		id : null,
+		username : "04182077",
+		headSculpture : 'https://dss0.bdstatic.com/6Ox1bjeh1BF3odCf/it/u=3097378621,1517430839&fm=74&app=80&f=JPEG&size=f121,90?sec=1880279984&t=7dcb802bd45e50b25868fc4d1302ffdc',
+		messageCount : null,
+		fans : null,
+		concern : null,
+		readCount : null,
+		studentId : "04182077"
+	}
+} 
